7fb10564ba420288cef3a530a08c1e631281e37a
LAPTOP-T1D3B800\ASUS <hoandoannguyen@gmail.com>
2021-06-05 11:42:57 +0700

    Enemy Take damaged, Damage deal through each hit, Atk damage in Animation Event, Fix collider for enemy, Atk prevent moving.

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,8 +7,13 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    //---------------- SerializeField zone ----------------------------
     [SerializeField]
     private LayerMask platformLayerMask;
+    
+    [SerializeField]
+    private LayerMask EnemyLayerMask;
 
     [SerializeField]
     private float movementSpd = 3f;
@@ -30,21 +35,33 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     float AtkingRate = 0.35f;
 
+    [SerializeField]
+    GameObject m_Atk1;
+
+    [SerializeField]
+    GameObject m_Atk2;
+
+    [SerializeField]
+    GameObject m_Atk3;
+
+    //---------------- Private zone ----------------------------
     float nextRollingTime = 0f;
     float AtkingTime = 0f;
-
     private int m_facingDirection = 1;
     private Animator m_animator;
     private Rigidbody2D m_rgbd2d;
     private BoxCollider2D m_boxCollider2D;
     private CircleCollider2D m_circleCollider2D;
     private SpriteRenderer m_spriteRenderer;
+    private Transform m_transform;
     private bool m_isAtking = false;
     private bool m_rolling = false;
     private bool m_isCrouch = false;
     private float m_delayToIdle = 0.0f;
     private string m_buttonPress;
 
+    //---------------- Public Zone ----------------------------
+    public Transform attackPoint;
     public TextMeshProUGUI Timer;
     public Text RollCooldownUI;
     public Image RollCooldownUIIcon;
@@ -66,6 +83,7 @@ public class PlayerMovement : MonoBehaviour
         m_circleCollider2D = GetComponent<CircleCollider2D>();
         m_animator = GetComponent<Animator>();
         m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_transform = GetComponent<Transform>();
     }
 
 
@@ -75,7 +93,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //--- Checking UI Rolling CD ---
         IsGrounded();
-        //CeilingCheck();
+        CeilingCheck();
         float remainingCD = nextRollingTime - Time.time;
         if (remainingCD > 0)
         {
@@ -93,16 +111,11 @@ public class PlayerMovement : MonoBehaviour
         Timer.text = "Timer: " + TimeCount;
 
         //====== Assign key ======
-        if ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) && IsGrounded() && !m_rolling && m_boxCollider2D.enabled)
-        {
-            m_isCrouch = true;
-            m_buttonPress = Crouch;
-        }
-        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !m_rolling && m_boxCollider2D.enabled)
+        if ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) && !m_rolling && m_boxCollider2D.enabled && Time.time >= AtkingTime)
         {
             m_buttonPress = Right;
         }
-        else if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && !m_rolling && m_boxCollider2D.enabled)
+        else if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) && !m_rolling && m_boxCollider2D.enabled && Time.time >= AtkingTime)
         {
             m_buttonPress = Left;
         }
@@ -149,52 +162,38 @@ public class PlayerMovement : MonoBehaviour
         //*** Crounching ***
         if (CeilingCheck())
         {
-            m_buttonPress = Crouch;
+            m_buttonPress = Roll;
         }
-        if (m_buttonPress == Crouch)
-        {
-            m_boxCollider2D.enabled = false;
-            m_animator.SetTrigger("Crouch");
-        }
-        
-
         //*** Moving RIGHT ***
         if (m_buttonPress == Right)
         {
-            GetComponent<SpriteRenderer>().flipX = false;
+            //GetComponent<SpriteRenderer>().flipX = false;
+            //flip with scaleX -1
+            m_transform.localScale = new Vector3(1, 1, 1);
             m_facingDirection = 1;
             m_rgbd2d.velocity = new Vector2(m_facingDirection * movementSpd, m_rgbd2d.velocity.y);
-            if (IsGrounded() && !m_rolling && m_rgbd2d.velocity.x != 0 && m_rgbd2d.velocity.y == 0)
+            if (IsGrounded() && !m_rolling)
             {
                 m_delayToIdle = 0.05f;
                 m_animator.SetInteger("AnimState", 1);
-            }else if (IsGrounded() && !m_rolling && m_rgbd2d.velocity.x != 0 && m_rgbd2d.velocity.y == 0 && m_isCrouch)
-            {
-                m_delayToIdle = 0.05f;
-                m_animator.SetInteger("AnimState", 2);
-                m_animator.SetTrigger("Crouch");
             }
         }
 
         //*** Moving LEFT ***
         else if (m_buttonPress == Left)
         {
-            GetComponent<SpriteRenderer>().flipX = true;
+            //GetComponent<SpriteRenderer>().flipX = true;
+            //flip with scaleX -1
+            m_transform.localScale = new Vector3(-1, 1, 1);
             m_facingDirection = -1;
             m_rgbd2d.velocity = new Vector2(m_facingDirection * movementSpd, m_rgbd2d.velocity.y);
-            if (IsGrounded() && !m_rolling && m_rgbd2d.velocity.x != 0 && m_rgbd2d.velocity.y == 0)
+            if (IsGrounded() && !m_rolling)
             {
                 m_delayToIdle = 0.05f;
                 m_animator.SetInteger("AnimState", 1);
             }
-            else if (IsGrounded() && !m_rolling && m_rgbd2d.velocity.x != 0 && m_rgbd2d.velocity.y == 0 && m_isCrouch)
-            {
-                m_delayToIdle = 0.05f;
-                m_animator.SetInteger("AnimState", 2);
-                m_animator.SetTrigger("Crouch");
-            }
         }
-
+        //*** Rolling ***
         else if (m_buttonPress == Roll)
         {
             m_rolling = true;
@@ -219,10 +218,10 @@ public class PlayerMovement : MonoBehaviour
             if (atkStyleChange > 3)
             {
                 atkStyleChange = 1;
-                m_rgbd2d.velocity = new Vector2(0, m_rgbd2d.velocity.y);
             }
             if (IsGrounded() && !m_rolling && m_isAtking)
             {
+                m_rgbd2d.velocity = new Vector2(0, m_rgbd2d.velocity.y);
                 //animator.Play("PlayerAtk");
                 m_animator.SetTrigger("Attack" + atkStyleChange);
                 atkStyleChange++;
@@ -230,7 +229,6 @@ public class PlayerMovement : MonoBehaviour
             }
             else if (!IsGrounded() && !m_rolling)
             {
-                m_rgbd2d.velocity = new Vector2(0, m_rgbd2d.velocity.y);
                 m_animator.SetTrigger("Attack3");
                 AtkingTime = Time.time + AtkingRate;
             }
@@ -242,7 +240,7 @@ public class PlayerMovement : MonoBehaviour
             // Prevents flickering transitions to idle
             m_delayToIdle -= Time.deltaTime;
             if (m_delayToIdle < 0)
-            m_animator.SetInteger("AnimState", 0);
+                m_animator.SetInteger("AnimState", 0);
             if (m_rolling)
             {
                 m_rolling = false;
@@ -251,15 +249,14 @@ public class PlayerMovement : MonoBehaviour
             {
                 m_isAtking = false;
             }
-            if(m_isCrouch)
+            if (m_isCrouch)
             {
                 m_isCrouch = false;
             }
-            if(Time.time >= AtkingTime)
+            if (Time.time >= AtkingTime)
             {
                 //m_rgbd2d.velocity = new Vector2(0, m_rgbd2d.velocity.y);
             }
-            //m_rgbd2d.velocity = new Vector2(0, m_rgbd2d.velocity.y);
         }
     }
 
@@ -292,18 +289,15 @@ public class PlayerMovement : MonoBehaviour
         Color rayColor;
         if (raycasthit.collider != null)
         {
-            m_animator.SetBool("ThereCeiling", true);
             rayColor = Color.cyan;
         }
         else
         {
-            m_animator.SetBool("ThereCeiling", false);
             rayColor = Color.yellow;
         }
         Debug.DrawRay(m_circleCollider2D.bounds.center + new Vector3(m_circleCollider2D.bounds.extents.x, 0), Vector2.up * (m_circleCollider2D.bounds.extents.y + extraHeightCeilCheck), rayColor);
         Debug.DrawRay(m_circleCollider2D.bounds.center - new Vector3(m_circleCollider2D.bounds.extents.x, 0), Vector2.up * (m_circleCollider2D.bounds.extents.y + extraHeightCeilCheck), rayColor);
         Debug.DrawRay(m_circleCollider2D.bounds.center - new Vector3(m_circleCollider2D.bounds.extents.x, m_circleCollider2D.bounds.extents.y), Vector2.right * (m_circleCollider2D.bounds.extents.x), rayColor);
-        Debug.LogWarning("Ceiling Check: " + raycasthit.collider);
         return raycasthit.collider != null;
     }
 
@@ -318,5 +312,29 @@ public class PlayerMovement : MonoBehaviour
     void AE_IdleVelocity()
     {
         m_rgbd2d.velocity = new Vector2(0, m_rgbd2d.velocity.y);
-    }    
+    }
+    void AE_AtkDamage1()
+    {
+        GameObject Atk = Instantiate(m_Atk1, attackPoint);
+    }
+    void AE_AtkDamage2()
+    {
+        GameObject Atk = Instantiate(m_Atk2, attackPoint);   
+    }
+    void AE_AtkDamage3()
+    {
+        GameObject Atk = Instantiate(m_Atk3, attackPoint);
+    }
+    void AE_AtkDamageEnd()
+    {
+        GameObject[] DestroyAtk = GameObject.FindGameObjectsWithTag("Slashing");
+        foreach (GameObject Atk in DestroyAtk)
+        {
+            Destroy(Atk);
+        }
+    }
+    void flip()
+    {
+            m_transform.localScale = new Vector3(-1,1,1);
+    }
 }
